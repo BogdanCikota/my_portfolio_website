204abb15835bf1b5a3903994616f624b7c842ad3
Bogdan Cikota <bogdanc@alphaefficiency.me>
2022-09-14 11:53:19 +0200

    Implement react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Link to="/faq">FAQ</Link>
     </div>
   );
 }
